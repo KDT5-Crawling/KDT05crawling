90270b7866919eb6b771d168856ae309d25f9bc7
winmin94 <155701485+winmin94@users.noreply.github.com>
2024-02-15 09:59:41 +0900

    crawlingTeamP

---
+++ 승민/감성분석.py
@@ -0,0 +1,82 @@
+
+from nltk.corpus import wordnet as wn
+
+# 간단한 NLTK PennTreebank Tag를 기반으로 WordNet 기반의 품사 Tag로 변환
+def penn_to_wn(tag):
+    if tag.startswith('J'):
+        return wn.ADJ
+    elif tag.startswith('N'):
+        return wn.NOUN
+    elif tag.startswith('R'):
+        return wn.ADV
+    elif tag.startswith('V'):
+        return wn.VERB
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import sentiwordnet as swn
+from nltk import sent_tokenize, word_tokenize, pos_tag
+
+def swn_polarity(text):
+    # 감성 지수 초기화
+    sentiment = 0.0
+    tokens_count = 0
+
+    lemmatizer = WordNetLemmatizer()
+    raw_sentences = sent_tokenize(text)
+    # 분해된 문장별로 단어 토큰 -> 품사 태깅 후에 SentiSynset 생성 -> 감성 지수 합산
+    for raw_sentence in raw_sentences:
+        # NTLK 기반의 품사 태깅 문장 추출
+        tagged_sentence = pos_tag(word_tokenize(raw_sentence))
+        for word , tag in tagged_sentence:
+
+            # WordNet 기반 품사 태깅과 어근 추출
+            wn_tag = penn_to_wn(tag)
+            if wn_tag not in (wn.NOUN , wn.ADJ, wn.ADV):
+                continue
+            lemma = lemmatizer.lemmatize(word, pos=wn_tag)
+            if not lemma:
+                continue
+            # 어근을 추출한 단어와 WordNet 기반 품사 태깅을 입력해 Synset 객체를 생성.
+            synsets = wn.synsets(lemma , pos=wn_tag)
+            if not synsets:
+                continue
+            # sentiwordnet의 감성 단어 분석으로 감성 synset 추출
+            # 모든 단어에 대해 긍정 감성 지수는 +로 부정 감성 지수는 -로 합산해 감성 지수 계산.
+            synset = synsets[0]
+            swn_synset = swn.senti_synset(synset.name())
+            sentiment += (swn_synset.pos_score() - swn_synset.neg_score())
+            tokens_count += 1
+
+    if not tokens_count:
+        return 0
+
+    # 총 score가 0 이상일 경우 긍정(Positive) 1, 그렇지 않을 경우 부정(Negative) 0 반환
+    if sentiment >= 0 :
+        return 1
+
+    return 0
+
+from googletrans import Translator
+import nltk
+#nltk.download('all')
+total_list =[]
+data_list = ['Artificial Intelligence','sad','happy']
+def translate_list_elements(input_list, target_language='en'):
+    translator = Translator()
+    translation_result = []
+    for element in input_list:
+        translation = translator.translate(element, dest=target_language).text
+        translation_result.append({element: translation})
+    return translation_result
+#print(data_list)
+for i in data_list:
+    translated_dict_list = translate_list_elements([i])
+    total_list.append(translated_dict_list)
+emotional_result =dict()
+for i in total_list:
+    if swn_polarity(list(i[0].values())[0]) :
+        emotional_result.setdefault(list(i[0].items())[0][0], "긍정")
+    else :
+        emotional_result.setdefault(list(i[0].items())[0][0], "부정")
+import pandas as pd
+df = pd.DataFrame(emotional_result.items(), columns=['title', 'result'])
+print(df)
